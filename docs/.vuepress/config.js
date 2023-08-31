4488a6e7abbda26bd4ae5542274927208fa2493a
liyao52033 <1503027549@qq.com>
2023-08-31 08:44:57 +0800

    增加algolia搜索
    Signed-off-by: liyao52033 <1503027549@qq.com>

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -47,14 +47,25 @@ module.exports = {
       apiKey: secureConf.algolia_apiKey,
       indexName: secureConf.algolia_indexName,
       appId: secureConf.algolia_appId,
+      container: "#search-form span #algolia-search-input",
       algoliaOptions: {
-        hitsPerPage: 1000,
+        hitsPerPage: 1000
       },
     },
     smoothScroll: true,
     sidebarDepth: 2,
     activeHeaderLinks: true,
     displayAllHeaders: false,
+    // 假如你的文档仓库和项目本身不在一个仓库：
+    docsRepo: 'https://github.com/liyao52033/vuepress-default',
+    // 假如文档不是放在仓库的根目录下：
+    docsDir: 'docs',
+    // 假如文档放在一个特定的分支下：
+    docsBranch: 'main',
+    // 默认是 false, 设置为 true 来启用
+    editLinks: true,
+    // 默认为 "Edit this page"
+    editLinkText: '帮助我们改善此页面!',
     lastUpdated: '上次更新'
   }
 
