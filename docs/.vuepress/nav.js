ff31b08d599c448ab04f7b1a2cdbc4ac6965eceb
liyao52033 <1503027549@qq.com>
2023-09-05 16:45:40 +0800

    first commit

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -1,43 +1,62 @@
-  const navbar = require("./gitnav.js")
-  module.exports = [
+const navbar = require("./gitnav.js")
+module.exports = [
   {
-    "text": "编程",
+    "text": "前端",
     "items": [
-    {
-      "text": "前端",
-      "link": "/pages/bafa79/"
-    },
-    {
-      "text": "后端",
-      "link": "/pages/17352a/"
-    }
-  ]
+      {
+        "text": "React",
+        "link": "/前端/react/"
+      },
+      {
+        "text": "Typescript",
+        "link": "/前端/typescript/"
+      },
+      {
+        "text": "Vue2",
+        "link": "/前端/vue2/vue-element-ui使用"
+      },
+      {
+        "text": "Vue3",
+        "link": "/前端/vue3/注意事项"
+      },
+      {
+        "text": "知识点",
+        "link": "/前端/"
+      }
+    ]
+  },
+
+  {
+    "text": "后端",
+    "items": [
+      {
+        "text": "Springboot",
+        "link": "/后端/springboot/MyBatis Plus使用"
+      }
+    ]
   },
 
   {
     "text": "项目实战",
     "items": [
-    {
-      "text": "在线判题系统",
-      "link": "/pages/55eef0/"
-    }
-  ]
+      {
+        "text": "在线判题系统",
+        "link": "/项目实战/在线判题系统/"
+      }
+    ]
   },
 
   {
     "text": "工具类",
-     link: '/archives/',
     "items": [
-    {
-      "text": "工具类",
-      "link": "/pages/a5da02/"
-    }
-  ]
+      {
+        "text": "工具类",
+        "link": "/工具类/常用网址"
+      }
+    ]
   },
 
+  navbar
 
 
-    navbar
-
-
-  ]
+]
