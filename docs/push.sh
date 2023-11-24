90b4e8f0abde7c4587e763325570a63a6333c49e
liyao52033 <1503027549@qq.com>
2023-11-24 11:33:54 +0800

    增加kiftd部署相关文章
    Signed-off-by: liyao52033 <1503027549@qq.com>

--- docs/push.sh
+++ docs/push.sh
@@ -4,7 +4,7 @@
 set -e
 git init
 git add -A
-git commit -m 'first commit'
+git commit -m '新增freemarker文章,完善tags标签'
 git branch -M main
 git push -f git@github.com:liyao52033/vuepress-default.git main
 
