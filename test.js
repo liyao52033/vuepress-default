6507b8c35cbc7185c6fab325beedf2132f65ea89
liyao52033 <1503027549@qq.com>
2023-11-30 23:50:02 +0800

    123

--- test.js
+++
@@ -1,9 +0,0 @@
-function test() {
-    if (typeof(window.console) == "undefined") {
-        window.console = {
-            log: function(msg) {
-                alert(msg);
-            }
-        };
-    }
-}
