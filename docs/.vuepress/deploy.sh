355384a3dc3e7aa07b8108dfa26f5856e9e7a8f2
liyao52033 <68154076+liyao52033@users.noreply.github.com>
2024-01-12 09:53:37 +0800

    Update deploy.sh

--- docs/.vuepress/deploy.sh
+++ docs/.vuepress/deploy.sh
@@ -3,12 +3,6 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-# 指定要使用的Node.js版本
-# nvm use 16.20.1
-
-#生成sitemap.xml
-yarn run sitemap
-
 # 生成静态文件
 yarn run docs:build
 
@@ -21,9 +15,6 @@ cd docs/.vuepress/dist
 # 复制readme
 cp ../../../docs/README.md .
 
-#复制sitemap.xml
-cp ../../../docs/sitemap.xml .
-
 # 如果是发布到自定义域名
 # echo 'xiaoying.org.cn' > CNAME
 
@@ -35,7 +26,8 @@ git branch -M main
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
-git push -f git@github.com:liyao52033/liyao52033.github.io.git main
+# 更改为自己的github地址
+# git push -f git@github.com:liyao52033/liyao52033.github.io.git main
 
 # 切换回18版本
 # nvm use 18
