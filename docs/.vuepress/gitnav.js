20cbe2d73e2dc4038ec97abd0d68f5f95b1d915b
liyao52033 <1503027549@qq.com>
2026-01-26 00:50:29 +0800

    feat: 移除valine评论插件，添加twikoo评论插件并更新相关配置；优化样式以提升用户体验

--- docs/.vuepress/gitnav.js
+++ docs/.vuepress/gitnav.js
@@ -15,7 +15,6 @@ module.exports = {
           text: "自动生成导航栏与侧边栏",
           link: "https://github.com/shanyuhai123/vuepress-plugin-auto-sidebar"
         },
-        { text: "评论系统", link: "https://github.com/dongyuanxin/vuepress-plugin-comment" },
         { text: "全文搜索", link: "https://github.com/meilisearch/vuepress-plugin-meilisearch" },
         { text: "选项卡", link: "https://github.com/lq782655835/vuepress-plugin-element-ui" },
         { text: "自动生成sitemap", link: "https://github.com/ekoeryanto/vuepress-plugin-sitemap" }
@@ -28,6 +27,7 @@ module.exports = {
           text: "批量操作frontmatter",
           link: "https://github.com/liyao52033/vuepress-plugin-editfrontmatter"
         },
+        { text: "评论系统", link: "https://github.com/liyao52033/vuepress-plugins/tree/main/vuepress-plugin-twikoo" },
         { text: "链接美化", link: "https://github.com/liyao52033/vuepress-plugin-replaceurl" },
         { text: "折叠代码块", link: "https://github.com/liyao52033/vuepress-plugins/tree/main/vuepress-plugin-blocktoggle" },
         { text: "复制代码块", link: "https://github.com/liyao52033/vuepress-plugins/tree/main/vuepress-plugin-copycodeblock" }
